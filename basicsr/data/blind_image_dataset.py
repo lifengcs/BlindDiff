0c2c9ed9898f3b216e5d618285cf81623c0e5317
Feng Li <l1feng@bjtu.edu.cn>
2024-06-26 17:54:28 +0800

    Add files via upload

---
+++ basicsr/data/blind_image_dataset.py
@@ -0,0 +1,84 @@
+from os import path as osp
+from torch.utils import data as data
+from torchvision.transforms.functional import normalize
+
+from basicsr.data.data_util import paths_from_lmdb
+from basicsr.data.transforms import augment, single_random_crop
+from basicsr.utils import FileClient, imfrombytes, img2tensor, scandir
+from basicsr.utils.registry import DATASET_REGISTRY
+
+
+@DATASET_REGISTRY.register()
+class BlindImageDataset(data.Dataset):
+    """Read only gt images in the train phase
+
+    Read GT.
+
+    There are two modes:
+    1. 'meta_info_file': Use meta information file to generate paths.
+    2. 'folder': Scan folders to generate paths.
+
+    Args:
+        opt (dict): Config for train datasets. It contains the following keys:
+            dataroot_gt (str): Data root path for gt.
+            meta_info_file (str): Path for meta information file.
+            io_backend (dict): IO backend type and other kwarg.
+    """
+
+    def __init__(self, opt):
+        super(BlindImageDataset, self).__init__()
+        self.opt = opt
+        # file client (io backend)
+        self.file_client = None
+        self.io_backend_opt = opt['io_backend']
+        self.mean = opt['mean'] if 'mean' in opt else None
+        self.std = opt['std'] if 'std' in opt else None
+        self.gt_folder = opt['dataroot_gt']
+
+        if self.io_backend_opt['type'] == 'lmdb':
+            self.io_backend_opt['db_paths'] = [self.gt_folder]
+            self.io_backend_opt['client_keys'] = ['gt']
+            self.paths = paths_from_lmdb(self.gt_folder)
+        elif 'meta_info_file' in self.opt:
+            with open(self.opt['meta_info_file'], 'r') as fin:
+                self.paths = [osp.join(self.gt_folder, line.split(' ')[0]) for line in fin]
+        else:
+            self.paths = sorted(list(scandir(self.gt_folder, full_path=True)))
+
+    def __getitem__(self, index):
+        scale = self.opt["scale"]
+        if self.file_client is None:
+            self.file_client = FileClient(self.io_backend_opt.pop('type'), **self.io_backend_opt)
+
+        # load gt image
+        gt_path = self.paths[index]
+        img_bytes = self.file_client.get(gt_path, 'gt')
+        img_gt = imfrombytes(img_bytes, float32=True)
+
+        # augmentation for training
+        if self.opt['phase'] == 'train':
+            gt_size = self.opt['gt_size']
+            # random crop
+            img_gt = single_random_crop(img_gt, gt_size, scale, gt_path)
+            # flip, rotation
+            img_gt = augment(img_gt, self.opt['use_flip'], self.opt['use_rot'])
+
+        if 'color' in self.opt and self.opt['color'] == 'y':
+            img_gt = rgb2ycbcr(img_gt, y_only=True)[..., None]
+
+        # crop the unmatched GT images during validation or testing, especially for SR benchmark datasets
+        # TODO: It is better to update the datasets, rather than force to crop
+        if self.opt['phase'] != 'train':
+            img_gt = img_gt[0:img_gt.shape[0] * scale, 0:img_gt.shape[1] * scale, :]
+
+        # BGR to RGB, HWC to CHW, numpy to tensor
+        img_gt = img2tensor(img_gt, bgr2rgb=True, float32=True)
+        # normalize
+        if self.mean is not None or self.std is not None:
+            normalize(img_gt, self.mean, self.std, inplace=True)
+            
+        return {'gt': img_gt, 'gt_path': gt_path}
+
+
+    def __len__(self):
+        return len(self.paths)
---
+++ basicsr/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Wed Jun 26 08:53:31 2024
+__version__ = '1.4.2'
+__gitsha__ = 'unknown'
+version_info = (1, 4, 2)
